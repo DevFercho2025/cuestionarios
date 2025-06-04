a906ed8360fe19aca52e0c09080cf3d51a08a650
Fernando Sanchez <fernando@sanchez.com>
2025-06-04 10:12:37 -0600

    ad

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware('check.user')->group(function () {
     Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('login', [AuthController::class, 'login'])->name('login.post');
 });
+
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/candidate', [CandidateController::class, 'index'])->name('candidate.index');
