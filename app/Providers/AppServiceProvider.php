29a2303c588c22b18874c07a4d41463f2bd87a0a
Fernando Sanchez <fernando@sanchez.com>
2025-06-04 10:01:12 -0600

    ad

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,22 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Personalizar redirección para usuarios autenticados
-        RedirectIfAuthenticated::redirectUsing(function ($request) {
-            $user = Auth::user();
-
-            // Si está en la app de cuestionarios (psicometrías)
-            if ($request->is('psicometricas/*') || $request->is('login')) {
-                // Verificar permisos de psico
-                if ($user->config && $user->config->is_psico_user && $user->config->active) {
-                    return '/psicometricas/admin';
-                }
-
-                Auth::logout();
-                return null; // Permite continuar al formulario de login
-            }
-
-            return '/dashboard';
-        });
+        //
     }
 }
